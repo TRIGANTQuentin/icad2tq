8c07015a11856415ddb0a47b3f0576bee7129d64
Quentin <trgt.quentin@hotmail.com>
2024-04-29 14:31:04 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/icad2admin/view/UpdateUserDialog.java

--- src/main/java/icad2admin/model/MySQLConnection.java
+++ src/main/java/icad2admin/model/MySQLConnection.java
@@ -15,9 +15,9 @@ import java.util.logging.Logger;
  */
 public class MySQLConnection {
 
-    private static final String url = "jdbc:mysql://localhost:3306/icad";
-    private static final String user = "root";
-    private static final String pass = "";
+    private static final String url = "jdbc:mysql://172.28.36.15/icad";
+    private static final String user = "quentin";
+    private static final String pass = "abzXea82*";
 
     private static Connection con = null;
 
